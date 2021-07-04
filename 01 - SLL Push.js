404d36a8b94073c20a395931e7846426c100f417
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-04 17:18:27 -0300

    new challange completed - SLL Push

---
+++ 01 - SLL Push.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
