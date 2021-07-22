47ced38d2cb9ac147bac80541293e715eff5a487
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-22 17:16:52 -0300

    new challange completed - stack pop

---
+++ 22 - Stack pop.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.size) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+  pop() {
+    if (!this.size) return 0;
+    let removedNode;
+
+    removedNode = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+}
+
+let stack = new Stack();
+
+stack.push(10);
+stack.push(100);
+
+stack.pop();
+stack.pop();
+
+console.log(stack);
