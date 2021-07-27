f010fd356444f31d123026cf2d1c71733ae85ea0
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-27 13:19:47 -0300

    new challange completed - enqueue

---
+++ 24 - Enqueue.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.size) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.size;
+  }
+}
+
+var queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(100);
+queue.enqueue(1000);
+
+console.log(queue);
