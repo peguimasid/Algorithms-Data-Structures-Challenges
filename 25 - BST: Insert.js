3f8755abd57e714ab282c3b05900a51b62e3bb06
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-28 16:51:07 -0300

    new challange completed - BST: insert

---
+++ 25 - BST: Insert.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (currentNode) {
+        if (newNode.value === currentNode) return undefined;
+
+        if (newNode.value > currentNode.value) {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+
+        if (newNode.value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        }
+      }
+    }
+
+    return this;
+  }
+}
+
+const bst = new BinarySearchTree();
+
+bst.insert(15).insert(20).insert(10).insert(12);
+
+console.log(bst.root.left);
