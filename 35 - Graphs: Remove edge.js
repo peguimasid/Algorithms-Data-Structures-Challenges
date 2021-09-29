165aba8d329cccb8545605b8a17254cbd29e837a
Guilhermo Masid <guilhermomasid@gmail.com>
2021-09-29 14:59:02 -0300

    new challange completed - graphs remove edge

---
+++ 35 - Graphs: Remove edge.js
@@ -0,0 +1,36 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
+}
+
+const graph = new Graph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'D');
+
+graph.removeEdge('A', 'B');
+
+console.log(graph);
