10ba8ac207aa97bdac3484b39054c744ba0db9b6
Guilhermo Masid <guilhermomasid@gmail.com>
2021-09-30 18:10:33 -0300

    new challenge completed - graphs remove vertex

---
+++ 36 - Graphs: Remove vertex.js
@@ -0,0 +1,44 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    for (let node of this.adjacencyList[vertex]) {
+      this.adjacencyList[node] = this.adjacencyList[node].filter(
+        (vtx) => vtx !== vertex
+      );
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+const graph = new Graph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'D');
+
+graph.removeVertex('C');
+
+console.log(graph);
